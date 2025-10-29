91baa7db66aa663df341e90bc1157a1ce9a1c61a
Lemon <khentalba1@gmail.com>
2025-10-29 18:48:07 +0800

    init

--- services/userService.ts
+++ services/userService.ts
@@ -1,25 +1,35 @@
+import { createId } from '@paralleldrive/cuid2';
 import bcrypt from "bcryptjs";
+import { eq } from 'drizzle-orm';
 
-import prisma from "@/lib/db";
-import { CreateUserSchema } from "@/lib/validators/user";
+import db from '@/lib/db';
+import { users } from '@/lib/db/schema';
+import { CreateUserSchema } from '@/lib/validators/user';
 
 export async function createUser(data: CreateUserSchema) {
   const hashedPassword = await bcrypt.hash(data.password, 10);
-  const {
-    password: _,
-    createdAt: __,
-    ...user
-  } = await prisma.user.create({
-    data: {
-      ...data,
+  const newUser = await db
+    .insert(users)
+    .values({
+      id: createId(),
+      email: data.email,
+      name: data.name,
       password: hashedPassword,
       birthday: new Date(data.birthday),
-    },
-  });
+      bio: data.bio,
+    })
+    .returning({
+      id: users.id,
+      email: users.email,
+      name: users.name,
+      birthday: users.birthday,
+      bio: users.bio,
+    });
 
-  return user;
+  return newUser[0];
 }
 
 export async function getUserByEmail(email: string) {
-  return prisma.user.findUnique({ where: { email } });
+  const result = await db.select().from(users).where(eq(users.email, email));
+  return result[0] || null;
 }
